ed3d194599b2f70d3a4d3ffe9b2f1b701743672f
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2022-08-04 21:54:17 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+<h1 class="mt-3 ">About ME</h1>
+<img src="<?= BASEURL; ?> /img/foto.png" alt="Rama Dhany" width="200" class="rounded-circle">
+        <p>Halo, nama saya <? $data['nama']; ?>, umur saya 18 <? $data['umur']; ?> tahun,saya adalah <?= $data['pekerjaan'] ?> </p>
+
+        
+</div>
